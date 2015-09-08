794ada3e228dc1dd862678344ecac64d4d81a3c7
easyhard <liuzhiyou.cs@gmail.com>
2015-09-08 10:26:08 +0800

    filelist component

--- filelist.jsx
+++ filelist.jsx
@@ -1,14 +1,22 @@
 var Components = Components || {};
 Components.FileList = React.createClass({
+    getInitialState: function () {
+        return {files: [
+            {title: "file title 1"},
+            {title: "file title 2"},
+            {title: "file title 3"},
+        ]};
+    },
     render: function () {
+        var buttons = this.state.files.map(function (file) {
+            return (
+               <button type="button" className={this.props.buttonClass + " list-group-item"} >{file.title}</button>
+            );
+        }.bind(this));
         return (
             <div className="list-group">
-               <button type="button" className="list-group-item">Cras justo odio</button>
-               <button type="button" className="list-group-item">Dapibus ac facilisis in</button>
-               <button type="button" className="list-group-item">Morbi leo risus</button>
-               <button type="button" className="list-group-item">Porta ac consectetur ac</button>
-               <button type="button" className="list-group-item">Vestibulum at eros</button>
+              {buttons}
             </div>
         );
-    }
+    },
 });
