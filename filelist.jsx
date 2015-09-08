5e0a0855520472f824ae0464d199803c10024be9
easyhard <liuzhiyou.cs@gmail.com>
2015-09-08 09:47:38 +0800

    filelist with react experiment

---
+++ filelist.jsx
@@ -0,0 +1,14 @@
+var Components = Components || {};
+Components.FileList = React.createClass({
+    render: function () {
+        return (
+            <div className="list-group">
+               <button type="button" className="list-group-item">Cras justo odio</button>
+               <button type="button" className="list-group-item">Dapibus ac facilisis in</button>
+               <button type="button" className="list-group-item">Morbi leo risus</button>
+               <button type="button" className="list-group-item">Porta ac consectetur ac</button>
+               <button type="button" className="list-group-item">Vestibulum at eros</button>
+            </div>
+        );
+    }
+});
